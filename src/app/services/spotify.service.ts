aa5288618f7aa0254a35732189f58346668f3d6c
David Noreña <danoco78@gmail.com>
2021-02-05 11:32:50 -0500

    serach v1

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,13 +11,23 @@ export class SpotifyService {
   getNewReleases(){
 
     const headers = new HttpHeaders({
-      Authorization: 'Bearer BQAlEagpKKFmKdDn-PU_NHbFapoSRHcGkA89mlBve1NX9mttwDNND1r61c7Plo2SOin6EhV4UsPJSJb5A2Q'
+      Authorization: 'Bearer BQDzWw4gtCIrLQ9KiD1qkkB7nOtM5BKrRkCRYdlpuTy5_hberIqJE2aVpM58aYDvOSmoP-RLB5n1o-XJc3Y'
     });
 
     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=25', {headers});
 
-      }
+  }
 
+  getArtista( termino: string) {
+    
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQDzWw4gtCIrLQ9KiD1qkkB7nOtM5BKrRkCRYdlpuTy5_hberIqJE2aVpM58aYDvOSmoP-RLB5n1o-XJc3Y'
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=20`, {headers});
+
+  }
+  
 }
 
 
